d1cbeefbe8a0ae1e33bca123a5cc2be0227e5a11
Nikhil Kherajani <104251165+Nikhil-Kherajani@users.noreply.github.com>
2022-11-13 22:14:19 +0530

    Update card.js

--- src/components/cards/card.js
+++ src/components/cards/card.js
@@ -8,7 +8,8 @@ function Card(){
                 <img src={image} className="card-img-top" alt="sorry" />
                     <div className="card-body">
                         <h5 className="card-title">Card title</h5>
-                        
+                        <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
                     </div>
                 </div>
                 </div>
